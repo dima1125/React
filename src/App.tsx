6ab8c69c4f3b088026029da5eb2c5f07e1e398f5
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-05-08 14:32:05 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
+import React from "react";
 import MyButton from "./components/Mybutton/MyButton";
 import Profile from "./components/Profile/Profile";
+import Header from "./components/Header";
+import Items from "./components/Items";
 
-function App() {
-  const isLogged = true;
-  let content;
-  if (isLogged) {
-    content = <Profile />;
-  } else {
-    content = null;
+class App extends React.Component {
+  constructor (props) {
+    super(props) 
+   this.state = {
+      items:[
+        {
+          id: 1,
+          name: 'Наручные часы мужские SKMEI 1251',
+          imageUrl: 'https://main-cdn.sbermegamarket.ru/big2/hlr-system/214/156/886/511/117/11/600004929632b0.jpeg',
+          price: '8165',
+          discount: '90',
+          rating: '4.7',
+          isFavorite: false
+          },  
+      ]
+    }
+  render() {
+    return (
+      <>
+        <Profile />
+        <Items items = {this.state}/>
+        <Header />
+      </>
+    )
   }
-  return (
-    <>
-      {content}
-      <MyButton />
-    </>
-  );
-}
-
+}}
 export default App;
---
+++ src/components/Items.tsx
@@ -0,0 +1,10 @@
+import React {Component} from "react";
+
+export class Items extends Component {
+  render() {
+    return (
+     |<div>Items</div>
+    );
+  }
+}
+export default Items
