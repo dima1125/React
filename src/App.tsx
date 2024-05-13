bbcc86cc6de331c1d88cef86731354097e81aef2
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-05-13 11:28:41 +0300

    lesson2-dz

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
 import React from "react";
 import MyButton from "./components/Mybutton/MyButton";
 import Profile from "./components/Profile/Profile";
-import Header from "./components/Header";
-import Items from "./components/Items";
+import Product from "./components/Mybutton/Product/Product";
 
-class App extends React.Component {
-  /* constructor (props) {
-    super(props) 
-   this.state = {
-      items:[
-        {
-          id: 1,
-          name: 'Наручные часы мужские SKMEI 1251',
-          imageUrl: 'https://main-cdn.sbermegamarket.ru/big2/hlr-system/214/156/886/511/117/11/600004929632b0.jpeg',
-          price: '8165',
-          discount: '90',
-          rating: '4.7',
-          isFavorite: false
-          },  
-      ]
-    } */
-  render() {
-    return (
-      <>
-        <Profile />
-
-        <Header />
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Profile />
+      <MyButton />
+      <Product />
+    </>
+  );
 }
 export default App;
