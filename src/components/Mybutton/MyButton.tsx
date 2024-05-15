fcc5684629799d2f55a87065e9277f34329d6968
Dmitriy Tolchkov <dmitrijj-tlchkv@rambler.ru>
2024-05-15 11:04:58 +0300

    lesson2 dz

--- src/components/Mybutton/MyButton.tsx
+++ src/components/Mybutton/MyButton.tsx
@@ -1,8 +1,19 @@
-const MyButton = () => {
+function AllertButton({ massage, children }) {
+  return <button onClick={() => alert(massage)}>{children}</button>;
+}
+export default function MyButton() {
+  return (
+    <>
+      <AllertButton massage="кнопка нажата">Кнопка</AllertButton>
+    </>
+  );
+}
+
+/* const MyButton = () => {
   const handleClick = () => {
     alert("Кнопка нажата");
   };
   return <button onClick={handleClick}>Кнопка</button>;
 };
 
-export default MyButton;
+export default MyButton; */
